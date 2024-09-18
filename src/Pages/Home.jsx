335bf616004509df7a9e54e22c6ef607ec176749
Saif Hasan <saifhassan399@gmail.com>
2024-09-19 00:45:20 +0600

    Carousel  , sponser section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import Carousel from '../components/Home/Carousel';
+import Sponser from '../components/Home/Sponser';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home</h2>
-            <h2 className="font-bold text-xl text-fuchsia-500 ">TailWind check</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is home</h2>
+      <h2 className="font-bold text-xl text-fuchsia-500 ">TailWind check</h2>
+
+      <Carousel></Carousel>
+      <Sponser></Sponser>
+    </div>
+  );
 };
 
 export default Home;
