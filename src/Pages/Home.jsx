2f25454639dc27d4dc7fa333d6135b492b0c9d65
Saif Hasan <saifhassan399@gmail.com>
2024-09-19 02:30:39 +0600

    LeaderCta section added on homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from '../components/Home/Hero';
 import Carousel from '../components/Home/Carousel';
 import Sponser from '../components/Home/Sponser';
+import LeaderCTA from '../components/Home/LeaderCTA';
 
 const Home = () => {
   return (
     <div>
       <h2>This is home</h2>
       <h2 className="font-bold text-xl text-fuchsia-500 ">TailWind check</h2>
+      <LeaderCTA></LeaderCTA>
     </div>
   );
 };
