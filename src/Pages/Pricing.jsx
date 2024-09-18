5425a4e12e4fc7c3aa3016e1d93b572331b66ee0
tofayeLL <ahmedtofayel132@gmail.com>
2024-09-18 19:18:52 +0600

    finished pricing card section

---
+++ src/Pages/Pricing.jsx
@@ -0,0 +1,14 @@
+import PriceCard from "../components/Pricing/PriceCard";
+
+
+const Pricing = () => {
+    return (
+        <section className="container mx-auto">
+           
+            <PriceCard></PriceCard>
+            
+        </section>
+    );
+};
+
+export default Pricing;
---
+++ src/components/Pricing/Faq.jsx
@@ -0,0 +1,12 @@
+
+
+const Faq = () => {
+    return (
+        <section>
+            <h1>From Faq</h1>
+            
+        </section>
+    );
+};
+
+export default Faq;
