947d4f5f157e4f9b6d3c51c5d9ed9ab2d963a4fd
nhnaahid <nhnahid.iu@gmail.com>
2024-09-19 01:28:16 +0600

    Added: call to action

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
+import CallToAction from "../components/Home/CallToAction";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <CallToAction></CallToAction>
     </div>
   );
 };
