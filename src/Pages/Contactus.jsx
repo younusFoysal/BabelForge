b606a8e07f5de0b6a1735663f7148506456ee398
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-09-19 10:36:30 +0600

    Fix: contact padding

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -6,13 +6,13 @@ const Contactus = () => {
   return (
     <section className="container mx-auto mt-10">
       {/* Parent div */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 lg:gap-20 p-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 lg:gap-20 p-3 ">
         {/* child 1 */}
         <div>
           <ContactForm />
         </div>
         {/* child 2 */}
-        <div>
+        <div className="lg:mt-0 mt-14">
           <ContactText />
         </div>
       </div>
