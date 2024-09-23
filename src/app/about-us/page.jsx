9cc3bb6c87e3a661ec5399879420d666f518a7ab
nhnaahid <nhnahid.iu@gmail.com>
2024-09-24 03:05:07 +0600

    create: about culture

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,3 +1,4 @@
+import AboutCulture from '@/components/AboutUs/AboutCulture';
 import AboutFirstBanner from '@/components/AboutUs/AboutFirstBanner';
 import AboutSecondBanner from '@/components/AboutUs/AboutSecondBanner';
 import AboutStatistics from '@/components/AboutUs/AboutStatistics';
@@ -9,6 +10,7 @@ const AboutUsPage = () => {
             <AboutFirstBanner></AboutFirstBanner>
             <AboutSecondBanner></AboutSecondBanner>
             <AboutStatistics></AboutStatistics>
+            <AboutCulture></AboutCulture>
         </div>
     );
 };
---
+++ src/components/AboutUs/AboutCulture.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutCulture = () => {
+    return (
+        <div>
+            <p>About Culture</p>
+        </div>
+    );
+};
+
+export default AboutCulture;
