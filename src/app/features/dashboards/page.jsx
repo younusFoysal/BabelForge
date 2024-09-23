eee6dee11f6fd0a6f9f7bf82c9faac6d94aba38f
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-09-23 18:18:45 +0600

    Add card components on dashboard

--- src/app/features/dashboards/page.jsx
+++ src/app/features/dashboards/page.jsx
@@ -1,9 +1,42 @@
+import DashboardCard from "@/components/DashboardsPage/DashboardCard";
 import Header from "@/components/DashboardsPage/Header";
+import card1 from "../../../image/dashboards/card1.png";
+import card2 from "../../../image/dashboards/card2.png";
+import card3 from "../../../image/dashboards/card3.png";
 
 const Dashboards = () => {
   return (
     <section>
       <Header />
+      <div className="container mx-auto">
+        <DashboardCard
+          row={"row"}
+          subtitle={"High-level overview"}
+          title={"Gain real-time insights"}
+          description={
+            "Easily analyze your data and simplify strategic decision-making with custom dashboards. Run reports, create summaries, track progress, and get a high-level overview of your entire organization."
+          }
+          image={card1}
+        />
+        <DashboardCard
+          row={"row-reverse"}
+          subtitle={"Dashboard customization"}
+          title={"View data your way"}
+          description={
+            "Build the reporting tools you need for your business with customizable no-code dashboards. Add widgets such as charts and timeline to help further visualize your data and stay up-to-date on progress and results."
+          }
+          image={card2}
+        />
+        <DashboardCard
+          row={"row"}
+          subtitle={"Resource management"}
+          title={"Prioritize work smarter"}
+          description={
+            "Adapt to changes and prioritize workloads strategically to boost team productivity and facilitate more efficient workflows. Quickly identify what needs your attention and catch potential risks before they happen."
+          }
+          image={card3}
+        />
+      </div>
     </section>
   );
 };
---
+++ src/components/DashboardsPage/DashboardCard.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+const DashboardCard = ({ row, subtitle, title, description, image }) => {
+  return (
+    <div
+      className={`flex flex-col md:flex-${row} items-center justify-evenly p-12`}
+    >
+      <div className="justify-center ">
+        <h6 className="text-blue-500">{subtitle}</h6>
+        <h1 className="text-3xl md:text-4xl font-bold my-4">{title}</h1>
+        <p className="w-[500px]">{description}</p>
+      </div>
+      <div>
+        <Image height={400} width={600} src={image} alt="card 1" />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardCard;
