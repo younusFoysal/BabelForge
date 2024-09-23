8aa5df8e46a2b4570533ef65cf895cc7e2363de4
nhnaahid <nhnahid.iu@gmail.com>
2024-09-24 01:07:28 +0600

    create: statistics section

---
+++ src/components/AboutUs/AboutStatistics.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const AboutStatistics = () => {
+    return (
+        <div>
+            <h1>babelforge.com by the numbers</h1>
+            <div>
+                <h3>2014</h3>
+                <p>Product launched</p>
+            </div>
+            <div>
+                <h3>1900+</h3>
+                <p>employees</p>
+            </div>
+            <div>
+                <h3>200+</h3>
+                <p>countries use <br /> babelforge.com</p>
+            </div>
+            <div>
+                <h3>2m</h3>
+                <p>boards actions are made every day</p>
+            </div>
+            <div>
+                <h3>225k+</h3>
+                <p>customers use monday.com to manage their work</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutStatistics;
