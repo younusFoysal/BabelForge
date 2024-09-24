45a5a2ad7c57b6e5fbcc35791747d272c25da341
tofayeLL <ahmedtofayel132@gmail.com>
2024-09-24 15:43:02 +0600

    finished dashboard sidebar menu by using shadcn

--- src/app/(dashboards)/dashboard/board/page.jsx
+++ src/app/(dashboards)/dashboard/board/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
+        <div >
             <Board></Board>        
         </div>
     );
--- src/components/Dashboards/Board.jsx
+++ src/components/Dashboards/Board.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import BoardCard from './BoardCard';
 
 const Board = () => {
     return (
-        <div>
-            <h1>I am from boards</h1>
-            
-        </div>
+        <section >
+
+            <div className='grid lg:grid-cols-3 grid-cols-3 lg:gap-6 gap-3'>
+                {/* card 1 */}
+                <div className='lg:w-[50vh] w-full'>
+                    <BoardCard
+                        title={"TO DO"}
+                    ></BoardCard>
+                </div>
+
+                {/* card 2 */}
+                <div className='lg:w-[50vh] w-full'>
+                    <BoardCard
+                        title={"IN PROGRESS"}
+                    ></BoardCard>
+                </div>
+
+                {/* card 3 */}
+                <div className='lg:w-[50vh] w-full'>
+                    <BoardCard
+                        title={"DONE"}
+                    ></BoardCard>
+                </div>
+
+            </div>
+
+        </section>
     );
 };
 
