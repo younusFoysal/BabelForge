2404b41da8a3a9b8511be96dc17042644b299cfd
Saif Hasan <saifhassan399@gmail.com>
2024-09-25 21:19:39 +0600

    Dashboard review carousel added

--- src/components/AboutUs/AboutCarousel.jsx
+++ src/components/AboutUs/AboutCarousel.jsx
@@ -4,13 +4,17 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
+import '@/components/AboutUs/AboutCarousel.css';
+import { MdOutlineEventAvailable } from 'react-icons/md';
+import { HiOutlineLightBulb } from 'react-icons/hi';
+import { RiCustomerService2Fill } from 'react-icons/ri';
 
 // Custom Arrow Icon
 function SampleNextArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="flex justify-center absolute z-30 hover:border border-[#fff] top-1/2 left-10 md:left-16 -translate-y-1/2 text-xl md:text-2xl items-center text-white rounded-full bg-black cursor-pointer w-10 h-10 md:w-14 md:h-14"
+      className="flex opacity-60 md:opacity-100 justify-center absolute z-30 hover:border border-[#fff] top-1/2 left-10 md:left-16 -translate-y-1/2 text-xl md:text-2xl items-center text-white rounded-full bg-black cursor-pointer w-10 h-10 md:w-14 md:h-14"
       onClick={onClick}
     >
       <FaArrowLeft />
@@ -22,7 +26,7 @@ function SamplePrevArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="flex justify-center absolute top-1/2 hover:border border-[#fff] right-10 md:right-16 z-30 -translate-y-1/2 text-xl md:text-2xl items-center text-white rounded-full bg-black cursor-pointer w-10 h-10 md:w-14 md:h-14"
+      className="flex opacity-60 md:opacity-100 justify-center absolute top-1/2 hover:border border-[#fff] right-10 md:right-16 z-30 -translate-y-1/2 text-xl md:text-2xl items-center text-white rounded-full bg-black cursor-pointer w-10 h-10 md:w-14 md:h-14"
       onClick={onClick}
     >
       <FaArrowRight />
@@ -31,18 +35,14 @@ function SamplePrevArrow(props) {
 }
 
 const AboutCarousel = () => {
-  // const customCarousel = {
-  //   opacity: '100',
-  //   boxShadow: '0px 0px 24px rgba(0,0,0,.101025)',
-  // };
-
   const settings = {
     dots: false,
+    className: 'centers',
     centerMode: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 2,
-    speed: 800,
+    speed: 500,
     swipeToSlide: true,
     centerPadding: '200px',
     nextArrow: <SamplePrevArrow />,
@@ -51,17 +51,17 @@ const AboutCarousel = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
-          centerMode: false,
+          centerMode: true,
         },
       },
       {
         breakpoint: 800,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           centerMode: false,
         },
       },
@@ -85,11 +85,11 @@ const AboutCarousel = () => {
   };
 
   return (
-    <div className=" relative w-screen mx-auto pt-8">
-      <h2 className="text-center mb-10 text-[24px] text-[#333] font-semibold">What would you like to manage?</h2>
-      <Slider {...settings}>
-        <div className="ml-4 md:ml-[90px] opacity-20  py-12 text-center  px-10 md:basis-1/2   rounded-xl">
-          <FaArrowRight className="text-[50px] mx-auto block mb-4" />
+    <div className="slider-container relative my-12 w-screen mx-auto pt-8">
+      <h2 className="text-center  text-[24px] text-[#333] font-semibold">What would you like to manage?</h2>
+      <Slider className="py-9 " {...settings}>
+        <div style={{ width: 200 }} className="my-12 carousel_div py-12   opacity-20 text-center px-8  md:px-14  rounded-xl">
+          <MdOutlineEventAvailable className="text-[55px] mx-auto block mb-4" />
           <p className="text-[#6161ff] mb-5 font-semibold text-3xl">Community Events</p>
           <p className="text-xl mb-4">
             We have launched a new series of events for our community , including live sessions with monday.com product managers. These
@@ -98,33 +98,24 @@ const AboutCarousel = () => {
           </p>
           <p className="font-semibold text-[18px]"> April 2024</p>
         </div>
-        <div className="ml-4 md:ml-[90px] opacity-20  py-12 text-center  px-10 md:basis-1/2   rounded-xl">
-          <FaArrowRight className="text-[50px] mx-auto block mb-4" />
-          <p className="text-[#6161ff] mb-5 font-semibold text-3xl">Community Events</p>
+        <div style={{ width: 200 }} className="my-12 carousel_div py-12  opacity-20 text-center px-8  md:px-14    rounded-xl">
+          <HiOutlineLightBulb className="text-[50px] mx-auto block mb-4" />
+          <p className="text-[#6161ff] mb-5 font-semibold text-3xl"> We are onto something...</p>
           <p className="text-xl mb-4">
-            We have launched a new series of events for our community , including live sessions with monday.com product managers. These
-            sessions are an opportunity for our community to connect directly with the team behind the features that they are using every
-            day.
+            The monday.com co-founders discovered deep, shared pain points across teams around the world and built a prototype for what is
+            known today as monday.com.
           </p>
           <p className="font-semibold text-[18px]"> April 2024</p>
         </div>
-        <div className="ml-4 md:ml-[90px] opacity-20  py-12 text-center  px-10 md:basis-1/2   rounded-xl">
-          <FaArrowRight className="text-[50px] mx-auto block mb-4" />
-          <p className="text-[#6161ff] mb-5 font-semibold text-3xl">Community Events</p>
+        <div style={{ width: 200 }} className="my-12 carousel_div py-12  opacity-20 text-center px-8  md:px-14    rounded-xl">
+          <RiCustomerService2Fill className="text-[50px] mx-auto block mb-4" />
+          <p className="text-[#6161ff] mb-5 font-semibold text-3xl"> Our first customers!</p>
           <p className="text-xl mb-4">
-            We have launched a new series of events for our community , including live sessions with monday.com product managers. These
-            sessions are an opportunity for our community to connect directly with the team behind the features that they are using every
-            day.
-          </p>
-          <p className="font-semibold text-[18px]"> April 2024</p>
-        </div>
-        <div className="ml-4 md:ml-[90px] opacity-20  py-12 text-center  px-10 md:basis-1/2   rounded-xl">
-          <FaArrowRight className="text-[50px] mx-auto block mb-4" />
-          <p className="text-[#6161ff] mb-5 font-semibold text-3xl">Community Events</p>
-          <p className="text-xl mb-4">
-            We have launched a new series of events for our community , including live sessions with monday.com product managers. These
-            sessions are an opportunity for our community to connect directly with the team behind the features that they are using every
-            day.
+            After two years, monday.com officially launched from a tiny “office” (but really a vacated apartment) in Tel Aviv and hosted its{' '}
+            <a className="text-[#6161ff]" href="#">
+              first board meeting
+            </a>{' '}
+            after onboarding our first 6 customers.
           </p>
           <p className="font-semibold text-[18px]"> April 2024</p>
         </div>
--- src/components/DashboardsPage/Review.jsx
+++ src/components/DashboardsPage/Review.jsx
@@ -1,5 +1,74 @@
+'use client';
+import { FaStar } from 'react-icons/fa6';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '@/components/DashboardsPage/Review.css';
+
 const Review = () => {
-  return <div></div>;
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: false,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+    arrows: false,
+    appendDots: dots => (
+      <div
+        style={{
+          borderRadius: '10px',
+          padding: '10px',
+        }}
+      >
+        <ul className="review_dots" style={{ margin: '0px' }}>
+          {dots}
+        </ul>
+      </div>
+    ),
+  };
+
+  return (
+    <section className="px-4 grid items-center grid-cols-1 md:grid-cols-8 gap-5 md:gap-20 container mx-auto pb-[90px]">
+      <div className="col-span-5 md:col-span-3 text-center md:text-left">
+        <h2 className="text-[35px] md:text-[45px] mb-4 font-extrabold text-[#333]">
+          Why customers <br /> love using our dashboards
+        </h2>
+        <p className="text-[#333] text-[16px] md:text-[20px] font-light">Based on 6000+ reviews on G2</p>
+      </div>
+      <div className="col-span-5">
+        <Slider {...settings}>
+          <div className="bg-gradient-to-b from-[#6161FF] to-[#6161FF] my-14 md:my-16  py-16 px-12 shadow-[#6161FF] rounded-xl shadow-2xl">
+            <blockquote className="font-light text-white text-[25px]">
+              “ Within minutes we can create dashboards to give a high-level, detailed view of where effort is being spent, and keep track
+              of time which simplifies follow-up and review.”
+            </blockquote>
+            <p className="text-white font-light text-[14px] my-5">Ray A. | Director of Customer Experience | Reviewed on G2 Crowd</p>
+            <div className="text-[28px] flex items-center gap-2">
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+            </div>
+          </div>
+
+          <div className="bg-gradient-to-b from-[#6161FF] to-[#6161FF] my-14 md:my-16  py-16 px-12 shadow-[#6161FF] rounded-xl shadow-2xl">
+            <blockquote className="font-light text-white text-[25px]">
+              “ BabelForge is a versatile project management tool, combining the best of Jira and Trello. It offers intuitive kanban boards,
+              time tracking, and detailed reporting”
+            </blockquote>
+            <p className="text-white font-light text-[14px] my-5">Ray A. | Director of Customer Experience | Reviewed on G2 Crowd</p>
+            <div className="text-[28px] flex items-center gap-2">
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
 };
 
 export default Review;
