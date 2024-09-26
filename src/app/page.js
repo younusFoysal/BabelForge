5c0fcd001ff312a84b41eadc5e94c4670cc91bba
morshidulrahman <boydanger416@gmail.com>
2024-09-27 03:30:12 +0600

    upd

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Hero from '@/components/home/Hero';
-import LeaderCTA from '@/components/home/LeaderCTA';
-import Sponser from '@/components/home/Sponser';
-import CallToAction from '@/components/home/CallToAction';
-import Carouselhome from '@/components/home/Carouselhome';
+import React from "react";
+import Hero from "@/components/home/Hero";
+import LeaderCTA from "@/components/home/LeaderCTA";
+import Sponser from "@/components/home/Sponser";
+import CallToAction from "@/components/home/CallToAction";
+import Carouselhome from "@/components/home/CarouselHome";
 import AboutStatistics from "@/components/AboutUs/AboutStatistics";
 
 const Home = () => {
   return (
     <div className="">
       <Hero />
-        <Carouselhome />
+      <Carouselhome />
       <Sponser />
-        <AboutStatistics></AboutStatistics>
+      <AboutStatistics></AboutStatistics>
       <LeaderCTA />
 
       <CallToAction />
-
     </div>
   );
 };
