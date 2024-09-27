061fb649c8d2b6670de0a77a9757d0ddd0033be5
morshidulrahman <boydanger416@gmail.com>
2024-09-28 00:50:45 +0600

    dark ui update

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,14 @@ import Hero from "@/components/home/Hero";
 import LeaderCTA from "@/components/home/LeaderCTA";
 import Sponser from "@/components/home/Sponser";
 import CallToAction from "@/components/home/CallToAction";
-import Carouselhome from "@/components/home/Carouselhome";
+
 import AboutStatistics from "@/components/AboutUs/AboutStatistics";
 
 const Home = () => {
   return (
     <div className="">
       <Hero />
-      <Carouselhome />
+
       <Sponser />
       <AboutStatistics></AboutStatistics>
       <LeaderCTA />
