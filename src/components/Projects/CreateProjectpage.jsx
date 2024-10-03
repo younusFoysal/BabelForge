b0e12f923e413b3dcce7d8207fcfa888705d21b5
younusFoysal <younus.faysal@gmail.com>
2024-10-04 01:32:58 +0600

    Deploy

--- src/components/Projects/CreateProjectpage.jsx
+++ src/components/Projects/CreateProjectpage.jsx
@@ -20,8 +20,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {router} from "next/client";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation"; // Correct import
 
 const projectCategories = [
   "All",
@@ -39,7 +38,7 @@ const CreateProjectpage = () => {
   const useremail = session?.data?.user?.email;
   const [emails, setEmails] = useState([useremail]);
   const axiosCommon = useAxiosCommon();
-  const [selectedCategory, setSelectedCategory] = useState(""); // Use state to store selected category
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const {
     register,
@@ -50,7 +49,6 @@ const CreateProjectpage = () => {
 
   useEffect(() => {
     const now = new Date();
-
     const gmt6Offset = 6 * 60 * 60 * 1000;
     const gmt6Date = new Date(now.getTime() + gmt6Offset);
 
@@ -68,10 +66,10 @@ const CreateProjectpage = () => {
       return res.data;
     },
     onSuccess: () => {
-      toast.success("Project send Successfully!");
+      toast.success("Project sent Successfully!");
       reset();
-      setEmails([]);
-      router.push("/dashboard/projects")
+      setEmails([useremail]); // Reset emails to include the user's email only
+      router.push("/dashboard/projects");
     },
     onError: () => {
       toast.error(`Something went wrong`);
@@ -90,136 +88,140 @@ const CreateProjectpage = () => {
   };
 
   return (
-    <div className="flex justify-between items-center flex-col">
-      <h2 className="text-4xl text-[#333] mb-4">Create projects</h2>
-      <div className="w-[60%]">
-        <div className="">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-[12px]">
-            <div className="mb-1">
-              <Label
-                htmlFor="tname"
-                className="text-left mb-2 block font-semibold"
-              >
-                Project Name <span className="text-red-600">*</span>
-              </Label>
-              <Input
-                {...register("pname", { required: true, minLength: 4 })}
-                placeholder="e.g. HR Team, Design Team"
-                id="pname"
-              />
-              {errors.pname?.type === "required" && (
-                <p className="text-red-600 mt-1">Project name required</p>
-              )}
-              {errors.pname?.type === "minLength" && (
-                <p className="text-red-600 mt-1">Name is too short!</p>
-              )}
-            </div>
+      <div className="flex justify-between items-center flex-col">
+        <h2 className="text-4xl text-[#333] mb-4">Create projects</h2>
+        <div className="w-[60%]">
+          <div className="">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-[12px]">
+              <div className="mb-1">
+                <Label
+                    htmlFor="tname"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Project Name <span className="text-red-600">*</span>
+                </Label>
+                <Input
+                    {...register("pname", { required: true, minLength: 4 })}
+                    placeholder="e.g. HR Team, Design Team"
+                    id="pname"
+                />
+                {errors.pname?.type === "required" && (
+                    <p className="text-red-600 mt-1">Project name required</p>
+                )}
+                {errors.pname?.type === "minLength" && (
+                    <p className="text-red-600 mt-1">Name is too short!</p>
+                )}
+              </div>
 
-            <div className="mb-1">
-              <Label
-                htmlFor="purl"
-                className="text-left mb-2 block font-semibold"
-              >
-                Project Url <span className="text-red-600">*</span>
-              </Label>
-              <Input
-                type="purl"
-                {...register("purl", { required: true })}
-                placeholder="Please enter your project URL"
-                id="purl"
-              />
-            </div>
+              <div className="mb-1">
+                <Label
+                    htmlFor="purl"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Project Url <span className="text-red-600">*</span>
+                </Label>
+                <Input
+                    type="text"
+                    {...register("purl", { required: true })}
+                    placeholder="Please enter your project URL"
+                    id="purl"
+                />
+                {errors.purl?.type === "required" && (
+                    <p className="text-red-600 mt-1">Project URL required</p>
+                )}
+              </div>
 
-            <div className="mb-1">
-              <Label
-                htmlFor="pimg"
-                className="text-left mb-2 block font-semibold"
-              >
-                Project Image Url <span className="text-red-600">*</span>
-              </Label>
-              <Input
-                type="pimg"
-                {...register("pimg", { required: true })}
-                placeholder="Please enter your project image URL"
-                id="pimg"
-              />
-            </div>
+              <div className="mb-1">
+                <Label
+                    htmlFor="pimg"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Project Image Url <span className="text-red-600">*</span>
+                </Label>
+                <Input
+                    type="text"
+                    {...register("pimg", { required: true })}
+                    placeholder="Please enter your project image URL"
+                    id="pimg"
+                />
+                {errors.pimg?.type === "required" && (
+                    <p className="text-red-600 mt-1">Project image URL required</p>
+                )}
+              </div>
 
-            <div className="mb-1">
-              <Label
-                htmlFor="pcategory"
-                className="text-left mb-2 block font-semibold"
-              >
-                Project Category <span className="text-red-600">*</span>
-              </Label>
-              <Select
-                onValueChange={(value) => setSelectedCategory(value)}
-                value={selectedCategory}
-              >
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select category" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    {projectCategories.map((category) => (
-                      <SelectItem value={category} key={category}>
-                        {category}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {errors.pcategory?.type === "required" && (
-                <p className="text-red-600 mt-1">Category required</p>
-              )}
-            </div>
+              <div className="mb-1">
+                <Label
+                    htmlFor="pcategory"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Project Category <span className="text-red-600">*</span>
+                </Label>
+                <Select
+                    onValueChange={(value) => setSelectedCategory(value)}
+                    value={selectedCategory}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select category" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {projectCategories.map((category) => (
+                          <SelectItem value={category} key={category}>
+                            {category}
+                          </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                {!selectedCategory && (
+                    <p className="text-red-600 mt-1">Category required</p>
+                )}
+              </div>
 
-            <div className="mb-1">
-              <Label
-                htmlFor="pdes"
-                className="text-left mb-2 block font-semibold"
-              >
-                Project Description <span className="text-red-600">*</span>
-              </Label>
-              <Textarea
-                {...register("pdes", { required: true, minLength: 10 })}
-                placeholder="Tell us about your project"
-                id="tdes"
-              />
-              {errors.pdes?.type === "required" && (
-                <p className="text-red-600 mt-1">Description required</p>
-              )}
-              {errors.pdes?.type === "minLength" && (
-                <p className="text-red-600 mt-1">Description too short!</p>
-              )}
-            </div>
+              <div className="mb-1">
+                <Label
+                    htmlFor="pdes"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Project Description <span className="text-red-600">*</span>
+                </Label>
+                <Textarea
+                    {...register("pdes", { required: true, minLength: 10 })}
+                    placeholder="Tell us about your project"
+                    id="tdes"
+                />
+                {errors.pdes?.type === "required" && (
+                    <p className="text-red-600 mt-1">Description required</p>
+                )}
+                {errors.pdes?.type === "minLength" && (
+                    <p className="text-red-600 mt-1">Description too short!</p>
+                )}
+              </div>
 
-            <div className="mb-1">
-              <Label
-                htmlFor="members"
-                className="text-left mb-2 block font-semibold"
-              >
-                Who should be in this project?
-              </Label>
-              <TagsInput
-                type="email"
-                id="members"
-                value={emails}
-                onChange={setEmails}
-                placeHolder="Enter emails"
-              />
-              <span>Press enter to add more</span>
-            </div>
+              <div className="mb-1">
+                <Label
+                    htmlFor="members"
+                    className="text-left mb-2 block font-semibold"
+                >
+                  Who should be in this project?
+                </Label>
+                <TagsInput
+                    value={emails}
+                    onChange={setEmails}
+                    placeHolder="Enter emails"
+                />
+                <span>Press enter to add more</span>
+              </div>
 
-            <div className="flex items-center gap-3 justify-end">
-              <Button className="" type="submit">
-                Create
-              </Button>
-            </div>
-          </form>
+              <div className="flex items-center gap-3 justify-end">
+                <Button className="" type="submit">
+                  Create
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
