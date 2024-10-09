15d2dbee1fc50e4d380588e82d5b90058a64238b
Saif Hasan <saifhassan399@gmail.com>
2024-10-09 23:49:17 +0600

    Admin dashboard ui fixed

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
-import {LuEye} from "react-icons/lu";
-import AdminStatCards from "@/components/Admin/AdminDashboard/AdminStatCards";
-import PaymentGraph from "@/components/Admin/AdminDashboard/PaymentGraph";
-import PaymentPieChart from "@/components/Admin/AdminDashboard/PaymentPieChart";
-import Transactions from "@/components/Admin/AdminDashboard/Transactions";
+import { LuEye } from 'react-icons/lu';
+import AdminStatCards from '@/components/Admin/AdminDashboard/AdminStatCards';
+import PaymentGraph from '@/components/Admin/AdminDashboard/PaymentGraph';
+import PaymentPieChart from '@/components/Admin/AdminDashboard/PaymentPieChart';
+import Transactions from '@/components/Admin/AdminDashboard/Transactions';
 
 const AdminDashboard = () => {
-    return (
-        <div>
-
-
-            <AdminStatCards/>
-
-
-            <div className="flex gap-3 justify-evenly w-full ">
-                <div
-                className="w-full">
-                    <PaymentGraph/>
-                </div>
-                <div className=" w-1/3">
-                    <PaymentPieChart/>
-                </div>
-            </div>
-
-
-            <Transactions/>
-
-
-
-
-
-        </div>
-    );
+  return (
+    <div className="px-4">
+      <AdminStatCards />
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-5">
+        <PaymentGraph />
+        <PaymentPieChart />
+      </div>
+      <Transactions />
+    </div>
+  );
 };
 
 export default AdminDashboard;
