f5260a0be93f4837db5a120238194010e13a6aae
Saif Hasan <saifhassan399@gmail.com>
2024-10-14 14:23:02 +0600

    Details card modified

--- src/components/Dashboards/MainPage/DetailsCard.jsx
+++ src/components/Dashboards/MainPage/DetailsCard.jsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import React, { useState } from 'react';
 import { RiTeamLine } from 'react-icons/ri';
