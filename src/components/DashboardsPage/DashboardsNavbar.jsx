598d43f9ecdb9c3abbc21efe0d9f8256c87dd7f9
Saif Hasan <saifhassan399@gmail.com>
2024-10-16 15:40:48 +0600

    Theme toggle buttton added

--- src/components/DashboardsPage/DashboardsNavbar.jsx
+++ src/components/DashboardsPage/DashboardsNavbar.jsx
@@ -1,47 +1,39 @@
-"use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import logo from "@/image/Home/babellogo.png";
-import axios from "axios";
-import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import {usePathname, useRouter} from "next/navigation";
-import { useState } from "react";
+'use client';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import logo from '@/image/Home/babellogo.png';
+import { signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { ModeToggle } from '../Theme/ModeToggle';
 const DashboardNavbar = () => {
   const pathname = usePathname();
   const session = useSession();
   const user = session?.data?.user;
   const router = useRouter();
-  //const [users, setUsers] = useState([]);
-
-
 
   const NavbarItems = [
     {
-      title: "Project",
-      href: "/dashboard/projects",
+      title: 'Project',
+      href: '/dashboard/projects',
     },
     {
-      title: "Teams",
-      href: "/dashboard/teams",
+      title: 'Teams',
+      href: '/dashboard/teams',
     },
     {
-      title: "Group Chat",
-      href: "/dashboard/chat",
+      title: 'Group Chat',
+      href: '/dashboard/chat',
     },
     {
-      title: "Dashboard",
-      href: "/dashboard",
+      title: 'Dashboard',
+      href: '/dashboard',
     },
     {
-      title: "Review",
-      href: "/dashboard/review",
-    }
+      title: 'Review',
+      href: '/dashboard/review',
+    },
   ];
 
   return (
@@ -58,14 +50,9 @@ const DashboardNavbar = () => {
         {/* Desktop Navigation */}
         <nav className="hidden md:flex">
           <ul className="flex space-x-6 items-center justify-center">
-            {NavbarItems.map((nav) => (
+            {NavbarItems.map(nav => (
               <Link href={nav.href} key={nav.href}>
-                <li
-                  className={`hover:text-blue-500 ${pathname === nav.href ? "text-blue-500 font-semibold" : ""
-                    }`}
-                >
-                  {nav.title}
-                </li>
+                <li className={`hover:text-blue-500 ${pathname === nav.href ? 'text-blue-500 font-semibold' : ''}`}>{nav.title}</li>
               </Link>
             ))}
           </ul>
@@ -73,39 +60,34 @@ const DashboardNavbar = () => {
 
         {/* Desktop Right Menu */}
         <div className="md:flex items-center space-x-4 mr-4">
+          <ModeToggle />
           {user && (
             <Popover>
               <PopoverTrigger>
                 <Avatar>
                   <AvatarImage
-                    src={
-                      user?.image
-                        ? user?.image
-                        : "https://getillustrations.b-cdn.net//photos/pack/3d-avatar-male_lg.png"
-                    }
+                    src={user?.image ? user?.image : 'https://getillustrations.b-cdn.net//photos/pack/3d-avatar-male_lg.png'}
                     className="w-16 h-16 rounded-full object-cover"
                   />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </PopoverTrigger>
-              <PopoverContent
-                  className="flex-col gap-2 p-4 flex dark:bg-gray-800 dark:border-gray-700 w-[200px] mr-4 mt-4">
+              <PopoverContent className="flex-col gap-2 p-4 flex dark:bg-gray-800 dark:border-gray-700 w-[200px] mr-4 mt-4">
                 <Link
-                    href="/dashboard/profile"
-                    className="bg-gray-100 py-2 px-4 w-full rounded-md text-center dark:text-white dark:bg-gray-900"
+                  href="/dashboard/profile"
+                  className="bg-gray-100 py-2 px-4 w-full rounded-md text-center dark:text-white dark:bg-gray-900"
                 >
                   profile
                 </Link>
                 <button
-                    onClick={async () => {
-                      await signOut();
-                      //router.push("/login");
-                    }}
-                    className="bg-gray-100 py-2 px-4 w-full rounded-md dark:text-white dark:bg-gray-900"
+                  onClick={async () => {
+                    await signOut();
+                    //router.push("/login");
+                  }}
+                  className="bg-gray-100 py-2 px-4 w-full rounded-md dark:text-white dark:bg-gray-900"
                 >
                   Logout
                 </button>
-
               </PopoverContent>
             </Popover>
           )}
