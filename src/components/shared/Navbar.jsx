bb57a7734f3e2d62b513734b55a9cd6cbe8d1358
Saif Hasan <saifhassan399@gmail.com>
2024-10-17 15:06:41 +0600

    Home page and feature page permanent dark mode added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,71 +1,63 @@
-"use client";
+'use client';
 
-import { AlignJustify, ArrowRight, X } from "lucide-react";
+import { AlignJustify, ArrowRight, X } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import logo from '../../image/Home/babellogo.png';
+import DashboardNavbar from '../DashboardsPage/DashboardsNavbar';
+import Button from './Buttons';
 
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
-import logo from "../../image/Home/babellogo.png";
-import DashboardNavbar from "../DashboardsPage/DashboardsNavbar";
-
-import Button from "./Buttons";
-
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  useAuth,
-  UserButton,
-} from "@clerk/nextjs";
-import { MdDashboard } from "react-icons/md";
+import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from '@clerk/nextjs';
+import { MdDashboard } from 'react-icons/md';
+import { useTheme } from 'next-themes';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { userId } = useAuth();
   const auth = !!userId;
-
+  const {} = useTheme();
   const pathname = usePathname();
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  if (pathname?.includes("sign-in") || pathname?.includes("sign-up"))
-    return null;
-  if (pathname?.includes("/successPayment")) return null;
+  if (pathname?.includes('sign-in') || pathname?.includes('sign-up')) return null;
+  if (pathname?.includes('/successPayment')) return null;
 
-  if (pathname?.includes("dashboard")) {
+  if (pathname?.includes('dashboard')) {
     return <DashboardNavbar />;
   }
-  if (pathname?.includes("stream")) {
+  if (pathname?.includes('stream')) {
     return null;
   }
 
   const NavbarItems = [
     {
-      title: "Features",
-      href: "/features",
+      title: 'Features',
+      href: '/features',
     },
     {
-      title: "Price",
-      href: "/pricing",
+      title: 'Price',
+      href: '/pricing',
     },
     {
-      title: "About Us",
-      href: "/about-us",
+      title: 'About Us',
+      href: '/about-us',
     },
     {
-      title: "Contact Us",
-      href: "/contact",
+      title: 'Contact Us',
+      href: '/contact',
     },
     {
-      title: "Video",
-      href: "/stream",
+      title: 'Video',
+      href: '/stream',
     },
   ];
 
   return (
-    <div className="bg-white/30 backdrop-blur-lg dark:bg-transparent border-b border-white/20 dark:border-gray-800/50 fixed w-full top-0 right-0 z-[999]">
+    <div className=" backdrop-blur-lg bg-transparent border-b  border-white/10 fixed w-full top-0 right-0 z-[999]">
       <div className="flex items-center justify-between container max-w-screen-2xl mx-auto px-4 py-4">
         {/* logo */}
         <Link href="/">
@@ -76,17 +68,11 @@ const Navbar = () => {
         </Link>
 
         {/* Desktop Navigation */}
-        <nav className="hidden md:flex">
+        <nav className="hidden text-white md:flex">
           <ul className="flex space-x-6 items-center justify-center">
-            {NavbarItems.map((nav) => (
+            {NavbarItems.map(nav => (
               <Link href={nav.href} key={nav.href}>
-                <li
-                  className={`hover:text-blue-500 ${
-                    pathname === nav.href ? "text-blue-500 font-semibold" : ""
-                  }`}
-                >
-                  {nav.title}
-                </li>
+                <li className={`hover:text-blue-500 ${pathname === nav.href ? 'text-blue-500 font-semibold' : ''}`}>{nav.title}</li>
               </Link>
             ))}
           </ul>
@@ -101,11 +87,7 @@ const Navbar = () => {
                   <UserButton>
                     <UserButton.MenuItems>
                       <UserButton.Action label="signOut" />
-                      <UserButton.Link
-                        label="Dashboard"
-                        href="/dashboard"
-                        labelIcon={<MdDashboard size={15} />}
-                      />
+                      <UserButton.Link label="Dashboard" href="/dashboard" labelIcon={<MdDashboard size={15} />} />
                       <UserButton.Action label="manageAccount" />
                     </UserButton.MenuItems>
                   </UserButton>
@@ -129,9 +111,7 @@ const Navbar = () => {
 
         {/* Mobile Menu Button */}
         <div className="flex md:hidden">
-          <button onClick={toggleMenu}>
-            {menuOpen ? <X size={30} /> : <AlignJustify size={30} />}
-          </button>
+          <button onClick={toggleMenu}>{menuOpen ? <X size={30} /> : <AlignJustify size={30} />}</button>
         </div>
       </div>
 
@@ -139,18 +119,14 @@ const Navbar = () => {
       {menuOpen && (
         <div className="md:hidden bg-white/30 backdrop-blur-lg border-t border-gray-200">
           <ul className="flex flex-col space-y-4 py-4">
-            {NavbarItems.map((nav) => (
+            {NavbarItems.map(nav => (
               <Link href={nav.href} key={nav.href}>
-                <li className="border-b border-gray-100 pb-3 px-6 hover:text-blue-500">
-                  {nav.title}
-                </li>
+                <li className="border-b border-gray-100 pb-3 px-6 hover:text-blue-500">{nav.title}</li>
               </Link>
             ))}
 
             <Link href="/login">
-              <li className="border-b border-gray-100 pb-3 px-6 hover:text-blue-500">
-                Login
-              </li>
+              <li className="border-b border-gray-100 pb-3 px-6 hover:text-blue-500">Login</li>
             </Link>
             <li>
               <div className="w-full items-center justify-center flex">
