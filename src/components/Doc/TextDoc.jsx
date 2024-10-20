8d5be4a9ba38a8ba2ac69e527a3566b6acdbe776
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-10-21 00:22:42 +0600

    Add: Toolbar options

--- src/components/Doc/TextDoc.jsx
+++ src/components/Doc/TextDoc.jsx
@@ -4,6 +4,20 @@ import Quill from "quill";
 import 'quill/dist/quill.snow.css';
 import { useCallback, useEffect, useRef } from "react";
 
+const TOOLBAR_OPTIONS = [
+  [{header: [1,2,3,4,5,6,false]}],
+  [{font: []}],
+  [{list: 'ordered'}, {list: 'bullet'}],
+  ['bold', 'italic', 'underline'],
+  [{color: []}, {background: []}],
+  [{script: 'sub'}, {script: 'super'}],
+  [{align: []}],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+  ['link'],
+  ['formula'],
+]
+
 const TextDoc = () => {
   
   const wrapperRef = useCallback((wrapper) => {
@@ -13,7 +27,10 @@ const TextDoc = () => {
     const editor = document.createElement('div');
     wrapper.append(editor);
     new Quill(editor,{
-      theme: 'snow'
+      theme: 'snow',
+      modules: {
+        toolbar: TOOLBAR_OPTIONS
+      }
     });
 
    
