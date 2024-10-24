84795af3b671e9f35a712bd11da84c300c3fa9d7
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-10-24 12:54:55 +0600

    Fix: carousel button clickable

--- src/components/shared/Buttons/ButtonSmall.jsx
+++ src/components/shared/Buttons/ButtonSmall.jsx
@@ -1,7 +1,10 @@
+import { useRouter } from "next/navigation";
+
 const ButtonSmall = ({ color, text }) => {
+  const Router = useRouter();
   return (
     <>
-      <button
+      <button onClick={()=>Router.push('/dashboard')}
         className={`bg-${color} dark:text-black font-light text-[13px] text-white py-[6px] px-6 rounded-full hover:bg-primary-dark transition-colors`}
       >
         {text}
