2297adee29775ecc0096713c681e5bee73c27328
tofayeLL <ahmedtofayel132@gmail.com>
2024-10-31 02:33:07 +0600

    update notes page design

--- src/app/(dashboards)/dashboard/notes/page.jsx
+++ src/app/(dashboards)/dashboard/notes/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Notes from "@/components/Notes/Notes";
+import Note from '@/components/Note/Note';
 
 const Page = () => {
     return (
         <>
 
-           <Notes/>
+          <Note></Note>
 
         </>
     );
