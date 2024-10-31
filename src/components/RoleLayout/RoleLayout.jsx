99df5c1657104a5badbb74e5b1ade3d428a3d958
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-11-01 01:06:03 +0600

    Fix: admin dashboard redirect issue

--- src/components/RoleLayout/RoleLayout.jsx
+++ src/components/RoleLayout/RoleLayout.jsx
@@ -1,12 +1,10 @@
 "use client"
-import React from 'react';
 import CripsHandle from "@/app/(dashboards)/dashboard/CripsHandle";
 import AdminDashboard from "@/components/Admin/AdminDashboard/AdminDashboard";
 import MainPageWrap from "@/components/Dashboards/MainPage/MainPageWrap";
 import useRole from "@/hooks/useRole";
 
 const RoleLayout = () => {
-
     const [role] = useRole();
 
     return (
