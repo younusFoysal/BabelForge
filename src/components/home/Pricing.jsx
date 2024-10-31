0fab51be306187357ebf2f9cc0a60f731bdb1165
nhnaahid <nhnahid.iu@gmail.com>
2024-10-31 22:30:18 +0600

    fix: homepage- pricing card

--- src/components/home/Pricing.jsx
+++ src/components/home/Pricing.jsx
@@ -18,62 +18,65 @@ const PricingHome = () => {
             <div className="pt-5 my-28" id="pricing">
                 <div className="mx-auto pb-20 mt-4 max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-4xl text-center">
-                        <h1 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h1>
-                        <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">Whether it's just
+                        <h1 className="text-base font-semibold leading-7 text-indigo-700 dark:text-indigo-400">Pricing</h1>
+                        <p className="mt-2 text-4xl font-bold tracking-tight text-black dark:text-white sm:text-5xl">Whether it's just
                             you, or your entire
                             team - we've got you covered.</p>
                     </div>
-                    <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">Choose the product
+                    <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-black dark:text-gray-300">Choose the product
                         that works best</p>
                     <div
                         className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        {
+                            packages.map((pack, idx) =>
+                                <div key={pack?._id} className={`${idx == 1 ? "bg-white/5 backdrop-blur-lg ring-2 ring-indigo-500 rounded-3xl p-8 xl:p-10" : "ring-1 ring-gray-400 dark:ring-white/30 backdrop-blur-lg rounded-3xl p-8 xl:p-10"}`}>
+                                    <div className="flex items-center justify-between gap-x-4">
+                                        <h2 id="product1" className="text-lg font-semibold leading-8 text-black dark:text-white">{pack?.title}</h2>
+                                    </div>
+                                    <p className="mt-4 text-sm leading-6 text-black dark:text-gray-300">{pack?.description}</p>
+                                    <p className="mt-6 flex items-baseline gap-x-1">
+                                        <span className="text-4xl font-bold tracking-tight text-black dark:text-white">$ {pack?.price} / {pack?.priceDetails}</span><span
+                                            className="text-sm font-semibold leading-6 text-gray-300"></span>
+                                    </p>
+                                    <a href="/order" aria-describedby="product1"
+                                        className="capitalize bg-gradient-to-r from-blue-600 to-purple-600  hover:shadow-purple-200 dark:hover:shadow-purple-800 dark:text-white  mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 text-white hover:scale-105 duration-300">View Details</a>
+                                    <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+                                        <li className="flex gap-x-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                                aria-hidden="true" className="h-6 w-5 flex-none text-black dark:text-white">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                            <p className='text-black dark:text-white'>{pack?.projects} Projects</p>
+                                        </li>
+                                        <li className="flex gap-x-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                                aria-hidden="true" className="h-6 w-5 flex-none text-black dark:text-white">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                            <p className='text-black dark:text-white'>{pack?.team} Teams</p>
+                                        </li>
+                                        <li className="flex gap-x-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                                aria-hidden="true" className="h-6 w-5 flex-none text-black dark:text-white">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                            <p className='text-black dark:text-white'>{pack?.task} Tasks</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            )
+                        }
 
-                        <div className="ring-1 ring-white/10 backdrop-blur-lg rounded-3xl p-8 xl:p-10">
-                            <div className="flex items-center justify-between gap-x-4">
-                                <h2 id="product1" className="text-lg font-semibold leading-8 text-white">Product Type
-                                    1</h2>
-                            </div>
-                            <p className="mt-4 text-sm leading-6 text-gray-300">Product details for Product Type 1</p>
-                            <p className="mt-6 flex items-baseline gap-x-1">
-                                <span className="text-4xl font-bold tracking-tight text-white">€ 10 / unit</span><span
-                                    className="text-sm font-semibold leading-6 text-gray-300"></span>
-                            </p>
-                            <a href="/order" aria-describedby="product1"
-                                className="bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Order
-                                Now</a>
-                            <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    40 units
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    1 feature
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    Fast delivery
-                                </li>
-                            </ul>
-                        </div>
 
+                        {/* test*/}
 
-                        <div className="bg-white/5 backdrop-blur-lg ring-2 ring-indigo-500 rounded-3xl p-8 xl:p-10">
+                        {/* <div className="bg-white/5 backdrop-blur-lg ring-2 ring-indigo-500 rounded-3xl p-8 xl:p-10">
                             <div className="flex items-baseline justify-between gap-x-4">
                                 <h2 id="product2" className="text-lg font-semibold leading-8 text-white">Product Type
                                     2</h2>
@@ -118,52 +121,11 @@ const PricingHome = () => {
                                     Fast delivery
                                 </li>
                             </ul>
-                        </div>
+                        </div> */}
 
 
-                        <div className="ring-1 ring-white/10 backdrop-blur-lg rounded-3xl p-8 xl:p-10">
-                            <div className="flex items-center justify-between gap-x-4">
-                                <h2 id="product3" className="text-lg font-semibold leading-8 text-white">Product Type
-                                    3</h2>
-                            </div>
-                            <p className="mt-4 text-sm leading-6 text-gray-300">Product details for Product Type 3</p>
-                            <p className="mt-6 flex items-baseline gap-x-1">
-                                <span className="text-4xl font-bold tracking-tight text-white">€ 50 / unit</span><span
-                                    className="text-sm font-semibold leading-6 text-gray-300"></span>
-                            </p>
-                            <a href="/order" aria-describedby="product3"
-                                className="bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Order
-                                Now</a>
-                            <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    240 units
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    6 different features
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true" className="h-6 w-5 flex-none text-white">
-                                        <path fill-rule="evenodd"
-                                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    Fast delivery
-                                </li>
-                            </ul>
-                        </div>
+
+
                     </div>
                 </div>
             </div>
