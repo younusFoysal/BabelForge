e35c0fd748eb3d4b2fba290f3905db051d655c81
morshidulrahman <boydanger416@gmail.com>
2024-11-03 18:47:08 +0600

    admin issue route

--- src/lib/axiosCommon.jsx
+++ src/lib/axiosCommon.jsx
@@ -1,11 +1,48 @@
+import { useUser } from "@clerk/nextjs";
 import axios from "axios";
 
+import { useEffect } from "react";
+import { useClerk } from "@clerk/nextjs";
+import { toast } from "@/hooks/use-toast";
 export const axiosCommon = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
-// Custom hook for using axios
 const useAxiosCommon = () => {
+  const { signOut } = useClerk();
+  const { user, isLoaded } = useUser();
+  useEffect(() => {
+    axiosCommon.interceptors.request.use(
+      function (config) {
+        const token = localStorage.getItem("access_token");
+
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+      },
+      function (error) {
+        return Promise.reject(error);
+      }
+    );
+
+    // response
+    axiosCommon.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
+        toast({
+          description: "Unauthorized access",
+          variant: "error",
+        });
+
+        if (error.status == 401 || error.status == 403) {
+          signOut({ redirectUrl: "/" });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [user, isLoaded]);
   return axiosCommon;
 };
 
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,36 +1,39 @@
 "use client";
 
-import useAxiosCommon from "@/lib/axiosCommon";
-import { useAuth, useUser } from "@clerk/nextjs";
-import { useQuery } from "@tanstack/react-query";
+import { useUser } from "@clerk/nextjs";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const AuthProvider = ({ children }) => {
-  const { isSignedIn, getToken } = useAuth();
-  const { user } = useUser();
-  const [loading, setLoading] = useState(true);
+  const { user, isLoaded } = useUser();
 
   useEffect(() => {
     const handleAuthState = async () => {
-      if (isSignedIn) {
-        const token = await getToken({ template: "my-template" }); // Use your custom token template if needed
-        localStorage.setItem("access_token", token);
-        setLoading(false);
+      if (user) {
+        const userInfo = { email: user?.primaryEmailAddress?.emailAddress };
+        const { data } = await axios.post(
+          "http://localhost:5000/api/jwttoken",
+          userInfo,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(data);
 
-        // Save user info if necessary
-        const userInfo = { email: user.primaryEmailAddress?.emailAddress };
-        await axios.post("/api/jwt", userInfo, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-      } else {
+        if (data.token) {
+          if (!localStorage.getItem("access_token")) {
+            localStorage.setItem("access_token", data.token);
+          }
+        }
+      }
+      if (!user || !isLoaded) {
         localStorage.removeItem("access_token");
-        setLoading(false);
+        return;
       }
     };
 
     handleAuthState();
-  }, [isSignedIn, getToken, user]);
+  }, [user, isLoaded]);
 
   return <div>{children}</div>;
 };
